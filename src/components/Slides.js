e0e8cf2092b38cb6239258e45aab5a34af271aa5
Anil Loutombam <nlkr50@gmail.com>
2020-12-03 11:41:15 +0530

    updated slides

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,97 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Slides({slides}) {
+function Slides({ slides }) {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [disabled, setDisabled] = useState({
+    prev: true,
+    next: false,
+    restart: true,
+  });
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
+  const nextSlide = () => {
+    let currentSlide = activeSlide;
 
+    if (currentSlide < slides.length - 1) {
+      currentSlide++;
+      setActiveSlide(currentSlide);
+      setDisabled({
+        ...disabled,
+        prev: false,
+        restart: false,
+      });
+    }
+    if (currentSlide === slides.length - 1) {
+      setDisabled({
+        ...disabled,
+        next: true,
+      });
+    }
+  };
+
+  const prevSlide = () => {
+    let currentSlide = activeSlide;
+
+    if (currentSlide > 0) {
+      currentSlide--;
+      setActiveSlide(currentSlide);
+      setDisabled({
+        ...disabled,
+        next: false,
+      });
+    }
+    if (currentSlide === 0) {
+      setDisabled({
+        ...disabled,
+        prev: true,
+        restart: true,
+      });
+    }
+  };
+
+  const onClickRestart = () => {
+    setActiveSlide(0);
+    setDisabled({
+      ...disabled,
+      prev: true,
+      next: false,
+      restart: true,
+    });
+  };
+
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          onClick={() => onClickRestart()}
+          disabled={disabled.restart}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          onClick={() => prevSlide()}
+          disabled={disabled.prev}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          onClick={() => nextSlide()}
+          disabled={disabled.next}
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title"> {slides[activeSlide].title} </h1>
+        <p data-testid="text"> {slides[activeSlide].title} </p>
+      </div>
+    </div>
+  );
 }
 
 export default Slides;
